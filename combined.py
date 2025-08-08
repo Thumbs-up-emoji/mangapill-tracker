8cb585a7eda3e30fcbc2863a56f84acb64574b86
Thumbs-up-emoji <f20221197@goa.bits-pilani.ac.in>
2025-08-08 14:51:44 +0530

    db_path

--- combined.py
+++ combined.py
@@ -1,21 +1,25 @@
 #if url- store, if number- update using new url, if blank- check all from db
 import sys
 from urllib.parse import urlparse
+import os
 from url import update
 from next import next_check
 from print import print_all
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(script_dir, 'db.csv')
 
 if len(sys.argv) > 1:
     arg = sys.argv[1]
     if urlparse(arg).scheme:
         # Code to handle when the argument is a URL
-        update(arg, 'db.csv')
+        update(arg, db_path)
     elif arg=='p':    
-        print_all('db.csv')
+        print_all(db_path)
     else:
         # Code to handle when the argument is neither a number nor a URL
         print("Error: Enter valid URL or manga no.")
 else:
     # Code to handle when no argument is provided
-    next_check('db.csv')
+    next_check(db_path)
     pass
